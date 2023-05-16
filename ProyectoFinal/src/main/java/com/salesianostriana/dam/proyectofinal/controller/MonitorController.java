7f1ebdf50ecb722bf3270e1e3a720e0d13ab5c3d
Pablo Cámara García <pcamaragar@gmail.com>
2023-05-16 09:55:45 +0200

    Borrado proyecto antiguo llamado ProyectoFinal, el bueno es ProyectoFinal-1

--- ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/controller/MonitorController.java
+++
@@ -1,8 +0,0 @@
-package com.salesianostriana.dam.proyectofinal.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MonitorController {
-
-}
--- ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/repos/JugadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.proyectofinal.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.proyectofinal.model.Jugador;
-
-public interface JugadorRepository extends JpaRepository<Jugador, Long>{
-
-}
