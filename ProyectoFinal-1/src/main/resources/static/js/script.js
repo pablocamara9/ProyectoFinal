61e8607bcb3b594662cb13a617fb4cbb7a236268
Pablo Cámara García <pcamaragar@gmail.com>
2023-05-18 12:42:32 +0200

    Añadida validación de formulario en formularioRegistro.js. Cambios en script.js, portada.html y los archivos html, css y js de formularioRegistro

--- ProyectoFinal-1/src/main/resources/static/js/script.js
+++ ProyectoFinal-1/src/main/resources/static/js/script.js
@@ -1,21 +1,23 @@
+//Método pause play vídeo
 let video = document.getElementById("miVideo")
+video.addEventListener('click', pausePlay)
 
-function pausePlay(video) {
-    if (video.paused) {
-        video.play()
-    }else{
-        video.pause()
-    }
+function pausePlay() {
+	if (this.paused) {
+		this.play()
+	} else {
+		this.pause()
+	}
 }
 
+//Método ver más y ver menos en el catálogo de productos
 let botonVerMas = document.getElementById('btnVerMas')
 let masCartas = document.getElementById('mas')
 
-
-function verMas(){
-        masCartas.style.display = "inline-block"
+function verMas() {
+	masCartas.style.display = "inline-block"
 }
 
 function verMenos() {
-        masCartas.style.display = "none"
-}   
+	masCartas.style.display = "none"
+}
