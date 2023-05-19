d1059d4e334d878ce5370f222609b125a7f4b1ac
Pablo Cámara García <pcamaragar@gmail.com>
2023-05-19 12:41:27 +0200

    Cambios en portada.html, añadido menú desplegable en el nav. Cambios en estilos.css, formularioRegistro.html, formularioInicioSesion.html y script.js

--- ProyectoFinal-1/src/main/resources/static/js/script.js
+++ ProyectoFinal-1/src/main/resources/static/js/script.js
@@ -12,6 +12,11 @@ function pausePlay() {
 
 //Método ver más y ver menos en el catálogo de productos
 let botonVerMas = document.getElementById('btnVerMas');
+let botonVerMenos = document.getElementById('btnVerMenos');
+
+botonVerMas.addEventListener('click', verMas);
+botonVerMenos.addEventListener('click', verMenos);
+
 let masCartas = document.getElementById('mas');
 
 function verMas(){
@@ -20,4 +25,11 @@ function verMas(){
 
 function verMenos() {
         masCartas.style.display = "none";
-}
+}   
+
+//Para ver más cuando pinche en uniforme de batalla y accesorios para las armas en el menú desplegable del nav
+/*let uniformeBatalla = document.getElementById('uniformeBatalla');
+let accesioriosArmas = document.getElementById('accesioriosArmas');
+
+uniformeBatalla.addEventListener('click', verMas);
+accesioriosArmas.addEventListener('click', verMas);*/
